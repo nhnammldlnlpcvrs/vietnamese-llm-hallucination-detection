9e2e627f5df423167d0be2b98e641f57d1a3f27d
Ho Nam <nghoofnam@gmail.com>
2026-01-27 21:02:29 +0700

    add: terraform for IaC

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,24 +1,19 @@
-resource "aws_eks_cluster" "main" {
-  name     = "hallucination-cluster"
-  role_arn = aws_iam_role.eks_cluster.arn
-  
-  vpc_config {
-    subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-  }
+# iac/terraform/main.tf
+
+provider "kubernetes" {
+  config_path = "~/.kube/config" 
 }
 
-resource "null_resource" "update_kubeconfig" {
-  depends_on = [aws_eks_cluster.main]
-
-  provisioner "local-exec" {
-    command = "aws eks update-kubeconfig --name ${aws_eks_cluster.main.name} --region your-region"
-  }
-}
-
-resource "null_resource" "ansible_provisioner" {
-  depends_on = [null_resource.update_kubeconfig]
-
-  provisioner "local-exec" {
-    command = "ansible-playbook -i ../ansible/inventory.ini ../ansible/setup_k8s_stack.yml"
+resource "kubernetes_namespace" "namespaces" {
+  for_each = toset([
+    "istio-system",
+    "knative-serving",
+    "kserve",
+    "observability",
+    "mlflow",
+    "hallucination-prod"
+  ])
+  metadata {
+    name = each.key
   }
 }
